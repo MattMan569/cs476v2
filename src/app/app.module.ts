3701c8aa8062190448005f06e751775227d3d3d1
Matthew Polsom <matthewpolsom@hotmail.com>
2019-03-15 15:43:08 -0600

    Added firebase imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LanguageTranslationModule } from './shared/modules/language-translation/language-translation.module'
+import { LanguageTranslationModule } from './shared/modules/language-translation/language-translation.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
+
+// Angular fire
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
 
 @NgModule({
     imports: [
@@ -15,6 +19,7 @@ import { AuthGuard } from './shared';
         BrowserModule,
         BrowserAnimationsModule,
         HttpClientModule,
+        AngularFireModule.initializeApp(environment.firebase),
         LanguageTranslationModule,
         AppRoutingModule
     ],

db8dd37677487f3f4725b476b0d037bd5cd0b633
Matthew Polsom <matthewpolsom@hotmail.com>
2019-03-17 00:42:36 -0600

    Modified interfaces

--- src/app/shared/interfaces.ts
+++ src/app/shared/interfaces.ts
@@ -1,26 +1,32 @@
 import { statusTypes, priorityTypes } from './types';
 
 export interface User {
-    email: string;
     uid: string;
+    email: string;
+    displayName: string;
 }
 
 export interface Project {
+    id: string; // https://stackoverflow.com/a/51403280
     name: string;
     description: string;
-    dateCreated: string;
-    dateDue: string;
-    dateCompleted: string;
+    dateCreated: firebase.firestore.Timestamp; // https://firebase.google.com/docs/reference/js/firebase.firestore.Timestamp
+    dateDue: firebase.firestore.Timestamp;
+    dateCompleted: firebase.firestore.Timestamp;
     status: statusTypes;
     priority: priorityTypes;
+    manager: number; // uid (creator)
 }
 
 export interface Task {
+    id: string;
+    projectId: string; // Project -> id
     description: string;
-    dateCreated: string;
-    dateDue: string;
-    dateCompleted: string;
+    dateCreated: firebase.firestore.Timestamp;
+    dateDue: firebase.firestore.Timestamp;
+    dateCompleted: firebase.firestore.Timestamp;
     weight: number;
     status: statusTypes;
     assignedTo: number; // uid
+    assignedBy: number; // uid
 }

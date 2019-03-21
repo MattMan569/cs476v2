68fc1dd342f4197b891198a6ec83cb9c6ddf0cdf
Matthew Polsom <matthewpolsom@hotmail.com>
2019-03-20 21:25:06 -0600

    Added task folder structure

--- src/app/layout/projects/projects.module.ts
+++ src/app/layout/projects/projects.module.ts
@@ -10,9 +10,25 @@ import { ProjectEditComponent } from './project-edit/project-edit.component';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectItemComponent } from './project-list/project-item/project-item.component';
 import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { TaskEditComponent } from './tasks/task-edit/task-edit.component';
+import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { TaskItemComponent } from './tasks/task-list/task-item/task-item.component';
 
 @NgModule({
     imports: [CommonModule, ReactiveFormsModule, NgbModule, ProjectsRoutingModule],
-    declarations: [ProjectsComponent, ProjectEditComponent, ProjectListComponent, ProjectItemComponent, ProjectDetailComponent]
+    declarations: [
+        ProjectsComponent,
+        ProjectEditComponent,
+        ProjectListComponent,
+        ProjectItemComponent,
+        ProjectDetailComponent,
+        TasksComponent,
+        TaskListComponent,
+        TaskEditComponent,
+        TaskDetailComponent,
+        TaskItemComponent
+    ]
 })
 export class ProjectsModule {}

f4ba3693e388587e1e21e19797beff5e10a96fa4
Matthew Polsom <matthewpolsom@hotmail.com>
2019-03-20 22:56:13 -0600

    Fixed error on page refresh

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-layout',
@@ -6,14 +9,36 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent implements OnInit {
+    private servicesLoaded: Promise<boolean>;
+    private afaSub: Subscription;
+    private afsSub: Subscription;
 
     collapedSideBar: boolean;
 
-    constructor() {}
+    constructor(private afa: AngularFireAuth, private afs: AngularFirestore) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.afaSub = this.afa.idToken.subscribe((id: string) => {
+            this.afaSub = this.afs
+                .collection('projects')
+                .valueChanges()
+                .subscribe(data2 => {
+                    this.servicesLoaded = Promise.resolve(true);
+                    if (this.afaSub) {
+                        this.afaSub.unsubscribe();
+                    }
+                    if (this.afsSub) {
+                        this.afsSub.unsubscribe();
+                    }
+                });
+        });
+    }
 
     receiveCollapsed($event) {
         this.collapedSideBar = $event;
+    }
+
+    isServicesLoaded(): Promise<boolean> {
+        return this.servicesLoaded;
     }
 }

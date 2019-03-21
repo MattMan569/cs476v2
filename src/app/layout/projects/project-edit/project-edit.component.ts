1029972a06217a11345fbf7c7581a447fa1fdfe2
Matthew Polsom <matthewpolsom@hotmail.com>
2019-03-20 21:06:05 -0600

    Prepopulate form on edit

--- src/app/layout/projects/project-edit/project-edit.component.ts
+++ src/app/layout/projects/project-edit/project-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { NgbDatepicker } from '@ng-bootstrap/ng-bootstrap';
-import { Router, ActivatedRoute } from '@angular/router';
-import { AngularFirestoreCollection, AngularFirestore } from '@angular/fire/firestore';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { Project } from 'src/app/shared';
 import { ProjectService } from 'src/app/shared/services/project.service';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -14,7 +14,8 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class ProjectEditComponent implements OnInit {
     private projectForm: FormGroup;
-    private projectsCollection: AngularFirestoreCollection<Project>;
+    private id: string;
+    private editMode = false;
 
     constructor(
         private afs: AngularFirestore,
@@ -25,14 +26,32 @@ export class ProjectEditComponent implements OnInit {
     ) {}
 
     ngOnInit() {
-        // Setup the form
-        this.projectForm = new FormGroup({
-            name: new FormControl(null, Validators.required),
-            description: new FormControl(null, Validators.required),
-            dateDue: new FormControl(null, Validators.required),
-            status: new FormControl('In Progress', Validators.required),
-            priority: new FormControl('Normal', Validators.required)
+        // Check if an existing form is being edited
+        this.route.params.subscribe((params: Params) => {
+            this.editMode = params.id != null;
+            this.id = params.id;
         });
+
+        if (this.editMode) {
+            // Setup a prepopulated form
+            const selectedProject = this.projectService.getProjectById(this.id);
+            this.projectForm = new FormGroup({
+                name: new FormControl(selectedProject.name, Validators.required),
+                description: new FormControl(selectedProject.description, Validators.required),
+                dateDue: new FormControl(selectedProject.dateDue, Validators.required),
+                status: new FormControl(selectedProject.status, Validators.required),
+                priority: new FormControl(selectedProject.priority, Validators.required)
+            });
+        } else {
+            // Setup a blank form
+            this.projectForm = new FormGroup({
+                name: new FormControl(null, Validators.required),
+                description: new FormControl(null, Validators.required),
+                dateDue: new FormControl(null, Validators.required),
+                status: new FormControl('In Progress', Validators.required),
+                priority: new FormControl('Normal', Validators.required)
+            });
+        }
 
         // Prevent the datepicker from moving the view
         NgbDatepicker.prototype.focus = function() {
@@ -58,15 +77,8 @@ export class ProjectEditComponent implements OnInit {
         newProject.dateCreated = Date.now();
         newProject.dateCompleted = null;
 
-        // Reference the project collection
-        this.projectsCollection = this.afs.collection('projects');
-
         // Add the document
         this.projectService.addProject(newProject);
-        // this.projectsCollection.add(newProject).then(ref => {
-        //     // Change the id field to the document id
-        //     ref.update({ id: ref.id });
-        // });
     }
 
     // Navigate up one level
